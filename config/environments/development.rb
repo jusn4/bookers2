94df5c4a8e629500024cbfb0ad8a73c04b880a9a
jusn4 <164776044+jusn4@users.noreply.github.com>
2024-04-19 10:19:09 +0000

    [Add]plugin,ActiveStorage,devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "9f98d977935f4ef69150f356beb5e776.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
