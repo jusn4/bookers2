4c2bd7b0a9fdc7bd147190731e440dd1c8a9a24c
EC2 Default User <ec2-user@ip-172-31-7-78.ap-northeast-1.compute.internal>
2024-04-22 01:30:54 +0000

    [Add]destroy

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :books, only: [:new,:create, :index, :show]
+  resources :books, only: [:new,:create, :index, :show,:destroy]
   resources :users, only: [:show, :edit, :index]
 end
